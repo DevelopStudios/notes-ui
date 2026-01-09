d81053879c716ba5bff02c29430f2913df5a576a
Charl <charlroux641@gmail.com>
2026-01-09 16:13:46 +0200

    Environement update

--- note-taking-app/src/environments/environment.prod.ts
+++ note-taking-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // *** NOTE: You must update this with your actual production API domain later! ***
-  apiUrl: 'https://your-production-api-domain.com/api/', 
+  apiUrl: 'https://notes-api-5eyf5.kinsta.app/api/', 
 };
